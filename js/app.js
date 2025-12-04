96d62dacdc7128b8adff0bf2a69309eade75649a
Dams2312 <225397863+Dams2312@users.noreply.github.com.>
2025-12-04 00:25:17 -0500

    feat: ✨ Finish

---
+++ js/app.js
@@ -0,0 +1,46 @@
+import { encoding_for_model } from "tiktoken";
+import fs from 'fs';
+
+async function procesamiento() {
+    
+    const encoding = encoding_for_model('gpt-4');
+
+    const libro = fs.createReadStream('/../Archivos/cien_años_de_soledad.txt', 'utf-8');
+
+    const tokens = encoding.encode(libro);
+
+    console.log(`Número total de tokens: ${tokens.length}`);
+
+    const maximo = 500;
+
+    let chunks = [];
+    let actual = [];
+
+    for (let token of tokens) {
+        if (actual.length >=  maximo) {
+            chunks.push(encoding.decode(actual));
+            actual = [];
+        }
+        actual.push(token);
+    }
+    if (chunkActual.length > 0) {
+        chunks.push(encoding.decode(chunkActual));
+    }
+    console.log(`Número total de fragmentos: ${chunks.length}`);
+
+    const costo = 0.03
+    let precio = 0
+
+    chunks.forEach((chunk, index) => {  
+
+        const numTokens = encoding.encode(chunk).length;
+        let preciochunk = (numTokens / 1000) * costo;
+        let total = precio + preciochunk;
+        precio = total;
+        console.log(`Fragmento ${index + 1}: ${numTokens} tokens, Costo: $${precio.toFixed(4)}`);
+    });
+    console.log(`Costo total estimado: $${precio.toFixed(4)}`);
+
+}
+procesamiento();
+
